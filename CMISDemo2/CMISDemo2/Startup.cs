9257156274295c6cf4e5e4e02a411faaf072959a
RkNjeru <35582018+RkNjeru@users.noreply.github.com>
2018-09-27 14:55:37 -0400

    Initial Demo Project

---
+++ CMISDemo2/CMISDemo2/Startup.cs
@@ -0,0 +1,106 @@
+﻿using CMISDemo2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cmisdemo2.startup))]
+namespace cmisdemo2
+{
+    public partial class startup
+    {
+        //public void configuration(IAppBuilder app)
+        //{
+        //    ConfigureAuth(app);
+        //}
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            //// Configure the db context and user manager to use a single instance per request
+            //app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            //// Enable the application to use a cookie to store information for the signed in user
+            //// and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            //// Configure the application for OAuth based flow
+            //PublicClientId = "self";
+            //OAuthOptions = new OAuthAuthorizationServerOptions
+            //{
+            //    TokenEndpointPath = new PathString("/api/Token"),
+            //    Provider = new ApplicationOAuthProvider(PublicClientId),
+            //    AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+            //    AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+            //    AllowInsecureHttp = true
+            //};
+
+            //// Enable the application to use bearer tokens to authenticate users
+            //app.UseOAuthBearerTokens(OAuthOptions);
+
+        }
+
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            createRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login   
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "TheManager";
+                user.Email = "TheManager@test.com";
+
+                string userPWD = "password";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Reader"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Reader";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
