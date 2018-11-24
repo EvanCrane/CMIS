e291238782db7ae0a31516626259d6af5e4de6b0
RkNjeru <35582018+RkNjeru@users.noreply.github.com>
2018-11-24 11:39:04 -0500

    Started working on Edit Collections Modals

--- cmis/src/main/resources/static/scripts/EditCollection.js
+++ cmis/src/main/resources/static/scripts/EditCollection.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
 
+    document.getElementById('addUser').on('click', function(event){
+        event.preventDefault();
+        document.getElementById('contactsModal').modal();
+
+    });
+
+    $('.editForm #addUser').on('click', function (event) {
+        event.preventDefault();
+       $('.contactFormModal #contactsModal').modal();
+
+    });
     $('.editForm .relationships').on('click', function (event) {
         event.preventDefault();
 
